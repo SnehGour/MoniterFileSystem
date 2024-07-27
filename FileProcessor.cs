b84ae65c44e5170751ba76fb7533030ace9a1d6a
snehgour80@gmail.com <snehgour80@gmail.com>
2024-07-27 17:10:06 +0530

    Initial

---
+++ FileProcessor.cs
@@ -0,0 +1,88 @@
+﻿using static System.Console;
+using System.IO;
+public class FileProcessor
+{
+    private string filePath;
+    private static readonly string BackUpDirectoryName = "backup";
+    private static readonly string InprogressDirectoryName = "inprogress";
+    private static readonly string CompletedDirectoryName = "completed";
+    public string InputFilePath { get; }
+    public FileProcessor(string filePath)
+    {
+        InputFilePath = filePath;
+    }
+    public void Process()
+    {
+        WriteLine($"Begin Process of {InputFilePath}");
+
+        // Check if file exists
+        if(!File.Exists(InputFilePath))
+        {
+            WriteLine($"Error: file doesnt exists");
+        }
+
+        string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+        WriteLine($"Root data path is {rootDirectoryPath}");
+
+        // Check if the backup dir exists
+        string inputFileDirPath = Path.GetDirectoryName(InputFilePath);
+        string backupDirPath = Path.Combine(rootDirectoryPath, BackUpDirectoryName);
+
+        if(!File.Exists(InputFilePath))
+        {
+            WriteLine($"File not Found in {inputFileDirPath}");
+            return;
+        }
+
+        if(!Directory.Exists(backupDirPath))
+        {
+            WriteLine($"Creating {backupDirPath}");
+            Directory.CreateDirectory(backupDirPath);
+        }
+
+
+        // Create backup: Copying files to backup directory
+        string inputFilePath = Path.GetFileName(InputFilePath);
+        string backupFilePath = Path.Combine(backupDirPath, inputFilePath);
+        WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+        File.Copy(InputFilePath, backupFilePath, true);     // true: Override the file
+
+
+        // Creating InProgress Directory
+        string inProgressDirPath = Path.Combine(rootDirectoryPath,InprogressDirectoryName);
+        if(!Directory.Exists(inProgressDirPath))
+        {
+            WriteLine($"Creating {inProgressDirPath}");
+            Directory.CreateDirectory(inProgressDirPath);
+        }
+
+        // Move input file to inprogess
+
+        // Check if file already there
+        string inprogressFilePath = Path.Combine(inProgressDirPath, inputFilePath);
+        if(File.Exists(inprogressFilePath))
+        {
+            WriteLine($"File is Already Processed !");
+            return;
+        }
+
+        WriteLine($"Moving File {inprogressFilePath} to {inprogressFilePath}");
+        File.Move(InputFilePath,inprogressFilePath);
+
+
+        // Once process is completed -> moving it to completed dir
+        string completedDirPath = Path.Combine(rootDirectoryPath, CompletedDirectoryName);
+
+        if(!Directory.Exists(completedDirPath))
+        {
+            WriteLine($"Creating {CompletedDirectoryName} at {completedDirPath}");
+            Directory.CreateDirectory(completedDirPath);
+        }
+
+        string completedFilePath = Path.Combine(completedDirPath, inputFilePath);
+        WriteLine($"Moving  file from {inprogressFilePath} to {completedFilePath}");
+        File.Move(inprogressFilePath, completedFilePath);
+
+        Directory.Delete(inProgressDirPath , true);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+public class Program
+{
+    public static void Main(string[]args)
+    {
+        WriteLine("Parsing COmmand Line Options");
+
+        var commands = args[0];
+        if(commands == "--file")
+        {
+            var filePath = args[1];
+            WriteLine($"Single file {filePath} selected");
+            ProcessSingleFile(filePath);
+        }
+        else if(commands == "--dir")
+        {
+            var directoryPath = args[1];
+            var fileType = args[2];
+            WriteLine($"Directory Path {directoryPath} selected for {fileType} files");
+            ProcessDirectory(directoryPath, fileType);
+        }
+        else
+        {
+            WriteLine("Invalid Command line options");
+        }
+        WriteLine("Press enter to quit");
+        ReadLine();
+    }
+    private static void ProcessSingleFile(string filePath)
+    {
+        var fileProcessor = new FileProcessor(filePath);
+        fileProcessor.Process();
+    }
+    private static void ProcessDirectory(string directoryPath,string filePath)
+    {
+        var allFiles = Directory.GetFiles(directoryPath);
+    }
+
+}
